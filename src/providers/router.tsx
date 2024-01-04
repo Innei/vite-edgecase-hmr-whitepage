e8674e081ba0169f1457e7a331087e2bb726d760
Innei <i@innei.in>
2024-01-04 18:46:03 +0800

    reproduction
    Signed-off-by: Innei <i@innei.in>

--- src/providers/router.tsx
+++ src/providers/router.tsx
@@ -1,6 +1,32 @@
-import { RouterProvider } from '@tanstack/react-router'
+import {
+  Outlet,
+  RootRoute,
+  Route,
+  Router,
+  RouterProvider,
+} from '@tanstack/react-router'
 
-import { router } from '../router'
+import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+
+export const rootRoute = new RootRoute({
+  component: () => (
+    <div>
+      <Outlet />
+
+      <TanStackRouterDevtools />
+    </div>
+  ),
+})
+
+const indexRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: '/',
+  component: () => <div>1xx1sxxxxxxxxx1</div>,
+})
+
+const routeTree = rootRoute.addChildren([indexRoute])
+
+export const router = new Router({ routeTree })
 
 declare module '@tanstack/react-router' {
   interface Register {
